091cfd00e4858f980dde240b325bee4b6de19216
Aroslav Kochkin <kochkin.2328@inbox.ru>
2025-11-14 20:42:51 +0700

    Создать и настроить HomeContentView

---
+++ TimelessTunesApp/Scenes/HomeScene/Views/HomeContentView.swift
@@ -0,0 +1,98 @@
+//
+//  HomeContentView.swift
+//  TimelessTunesApp
+//
+//  Created by Ярослав Кочкин on 14.11.2025.
+//
+
+import UIKit
+import SnapKit
+
+protocol IHomeContentView: UIView {}
+
+final class HomeContentView: UIView {
+	
+	private var tracks: [TrackModel] = []
+	
+	private let reuseIdentifier = "listReuseIdentifier"
+	private var collectionView: UICollectionView!
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setup()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+}
+
+private extension HomeContentView {
+	
+	func setup() {
+		backgroundColor = .lightGray
+		setupCollectionView()
+	}
+	
+	func setupCollectionView() {
+		collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+		collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+		collectionView.backgroundColor = UIColor(resource: .background)
+		collectionView.dataSource = self
+		collectionView.delegate = self
+		addSubview(collectionView)
+
+		collectionView.snp.makeConstraints { make in
+			make.edges.equalTo(safeAreaLayoutGuide)
+		}
+	}
+	
+	func createLayout() -> UICollectionViewLayout {
+		let itemSize = NSCollectionLayoutSize(
+			widthDimension: .fractionalWidth(0.5),
+			heightDimension: .absolute(200)
+		)
+		let item = NSCollectionLayoutItem(layoutSize: itemSize)
+		let groupSize = NSCollectionLayoutSize(
+			widthDimension: .fractionalWidth(1),
+			heightDimension: .absolute(220)
+		)
+		let group = NSCollectionLayoutGroup.horizontal(
+			layoutSize: groupSize,
+			subitems: [item]
+		)
+		group.interItemSpacing = .fixed(25)
+		let section = NSCollectionLayoutSection(group: group)
+		section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 24, bottom: 10, trailing: 24)
+		return UICollectionViewCompositionalLayout(section: section)
+	}
+	
+}
+
+extension HomeContentView: UICollectionViewDataSource {
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		10
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		guard let cell = collectionView.dequeueReusableCell(
+			withReuseIdentifier: reuseIdentifier,
+			for: indexPath
+		) as? HomeCollectionViewCell else {
+			return UICollectionViewCell()
+		}
+		cell.configure()
+		return cell
+	}
+	
+	
+}
+
+extension HomeContentView: UICollectionViewDelegate {
+	
+}
+
+extension HomeContentView: IHomeContentView {
+	
+}
