de77dceda0be8e467ac40aa84145f6108a29538e
Aroslav Kochkin <kochkin.2328@inbox.ru>
2025-11-14 20:44:04 +0700

    Добавил HomeEntity

---
+++ TimelessTunesApp/Scenes/HomeScene/HomeEntity.swift
@@ -0,0 +1,13 @@
+//
+//  HomeEntity.swift
+//  TimelessTunesApp
+//
+//  Created by Ярослав Кочкин on 14.11.2025.
+//
+
+import Foundation
+
+struct TrackModel {
+	let trackName: String
+	let artistName: String
+}
