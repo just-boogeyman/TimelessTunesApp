802211d5b4baac699708452901c39be399639d84
Aroslav Kochkin <kochkin.2328@inbox.ru>
2025-11-15 22:28:43 +0700

    Настройка Assembler

--- TimelessTunesApp/Scenes/HomeScene/HomeAssembler.swift
+++ TimelessTunesApp/Scenes/HomeScene/HomeAssembler.swift
@@ -11,14 +11,17 @@ import UIKit
 enum HomeAssembler {
 	
 	struct Dependencies {
+		let networkService: INetworkService
 		let router: IHomeRouter
 	}
 	
 	static func assembly(dependencies: Dependencies) -> UIViewController {
 		
-		let presenter = HomePresenter(router: dependencies.router)
+		let interactor = HomeInteractor(networkService: dependencies.networkService)
+		let presenter = HomePresenter(interactor: interactor, router: dependencies.router)
 		let viewController = HomeViewController(presenter: presenter)
 		
+		interactor.output = presenter
 		return viewController
 	}
 }
