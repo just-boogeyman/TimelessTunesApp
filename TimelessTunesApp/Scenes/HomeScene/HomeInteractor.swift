ba57dff360cb8db498dbeeaf618c59879add0f8c
Aroslav Kochkin <kochkin.2328@inbox.ru>
2025-11-15 22:26:34 +0700

    Настроил интерактор для работы с сетью

--- TimelessTunesApp/Scenes/HomeScene/HomeInteractor.swift
+++ TimelessTunesApp/Scenes/HomeScene/HomeInteractor.swift
@@ -8,8 +8,39 @@
 import Foundation
 
 
-protocol IHomeInteractor {}
+protocol IHomeInteractorInput {
+	func searchTracks(searchText: String)
+}
+
+protocol IHomeInteractorOutput: AnyObject {
+	func presentData(responce: [MediaItem])
+	func didFail(error: Error)
+}
 
 final class HomeInteractor {
+	
+	private let networkService: INetworkService
+	weak var output: IHomeInteractorOutput?
+	 
+	init(networkService: INetworkService) {
+		self.networkService = networkService
+	}
+
+}
+
+extension HomeInteractor: IHomeInteractorInput {
+	func searchTracks(searchText: String) {
+		networkService.loadData(form: .basic, ITunesSearchResponse.self, string: searchText) { [weak self] result in
+			guard let self else { return }
+			switch result {
+			case .success(let response):
+				let entities = response.results.map { $0.toEntity() }
+				output?.presentData(responce: entities)
+			case .failure(let error):
+				output?.didFail(error: error)
+			}
+		}
+	}
+	
 	
 }
