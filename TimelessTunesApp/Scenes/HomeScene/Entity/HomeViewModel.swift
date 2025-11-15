9667225b62bfa40b224509c89650c32bb680ff30
Aroslav Kochkin <kochkin.2328@inbox.ru>
2025-11-15 22:26:07 +0700

    Добавил Entities

---
+++ TimelessTunesApp/Scenes/HomeScene/Entity/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewModel.swift
+//  TimelessTunesApp
+//
+//  Created by Ярослав Кочкин on 15.11.2025.
+//
+
+import Foundation
+
+struct HomeViewModel {
+	let iconUrlString: String?
+	let trackName: String
+	let artistName: String
+}
---
+++ TimelessTunesApp/Scenes/HomeScene/Entity/MediaItem.swift
@@ -0,0 +1,15 @@
+//
+//  HomeEntity.swift
+//  TimelessTunesApp
+//
+//  Created by Ярослав Кочкин on 14.11.2025.
+//
+
+import Foundation
+
+struct MediaItem {
+	let id: Int
+	let artistName: String
+	let trackName: String
+	let artworkUrl: String?
+}
