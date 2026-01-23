f05f93fd46f9c7a14afb31a5c0f353111f45524b
Aroslav Kochkin <kochkin.2328@inbox.ru>
2026-01-23 10:41:35 +0700

    tim-5

--- TimelessTunesApp/Scenes/DetailTrackScene/View/DetailTrackViewController.swift
+++ TimelessTunesApp/Scenes/DetailTrackScene/View/DetailTrackViewController.swift
@@ -25,6 +25,6 @@ final class DetailTrackViewController: UIViewController, IDetailTrackViewControl
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = .white
+		view.backgroundColor = .red
 	}
 }
